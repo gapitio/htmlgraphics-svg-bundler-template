2dc32b7916bddf723a57918ac1f4e5a8ff9bb7d4
ZuperZee <ole.kristian.kaaseth@gmail.com>
2021-02-03 13:01:54 +0100

    fix: fix onpanelupdate missing from htmlNode

--- src/dev-site/html-node.ts
+++ src/dev-site/html-node.ts
@@ -2,6 +2,7 @@ import svgData from "../design/svg-data.svg";
 
 window.htmlNode = document
   .getElementById("shadow-container")
-  .attachShadow({ mode: "open" });
+  .attachShadow({ mode: "open" }) as HTMLNode;
 
+htmlNode.onpanelupdate = () => null;
 htmlNode.innerHTML = `<style></style><div>${svgData}</div>`;
